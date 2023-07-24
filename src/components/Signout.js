a84d718b746c7f10ea762af3989c0c1fbf5880f9
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-07-24 14:42:27 +0530

    updatecodes

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -1,35 +1,54 @@
-import React, { useEffect, useContext } from "react";
-import {  useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const Signout = () => {
   const history = useHistory();
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
 
   useEffect(() => {
-    fetch("https://bike-rental-portal-k5bj.onrender.com/signout", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
-      .then((res) => {
-        localStorage.removeItem("User");
-        history.push("/signin", { replace: true });
-        window.location.reload();
-        if (res.status != 200) {
-          const error = new Error(res.error);
+    const logoutUser = async () => {
+      try {
+        const response = await fetch("https://bike-rental-portal-k5bj.onrender.com/signout", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+
+        if (response.status !== 200) {
+          const error = new Error("Failed to logout.");
           throw error;
         }
-      })
-      .catch((error) => {
+
+        localStorage.removeItem("User");
+        setShowSuccessModal(true);
+      } catch (error) {
         console.log(error);
-      });
-  });
+      }
+    };
+
+    logoutUser();
+  }, []);
+
+  const handleModalClose = () => {
+    setShowSuccessModal(false);
+    history.push("/");
+  };
 
   return (
     <>
       <h1>Log Out</h1>
+      {/* Modal to show success message */}
+      {showSuccessModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Successfully logged out!</p>
+            <button onClick={handleModalClose}>OK</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
